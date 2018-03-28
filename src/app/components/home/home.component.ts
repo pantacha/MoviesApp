951a50a7eb9535695da2fc2c1289dea2c01633d8
pantacha <pantacha@hotmail.com>
2018-03-28 11:53:47 +0200

    Diseño de la pagina Home y actualizacion del service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmsService } from '../../services/films.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _filmsService: FilmsService) {
+    this._filmsService.getCartelera()
+                                .subscribe((data) => {
+                                  console.log(data);
+                                })
+  }
 
   ngOnInit() {
   }
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -5,6 +5,25 @@ import 'rxjs/Rx';
 @Injectable()
 export class FilmsService {
 
-  constructor() { }
+  private apiKey: string = "1789561a62fd413a0c2bc8b30e1ef9bf";
+  private urlMoviedb: string = "https://api.themoviedb.org/3";
+
+  constructor(private jsonp: Jsonp) {
+
+  }
+
+  getCartelera(){
+    let desde = new Date();
+    let hasta = new Date();
+    hasta.setDate(hasta.getDate() + 7);
+
+    let desdeStr = `${desde.getFullYear()}-${desde.getMonth()+1}-${desde.getDate()}`;
+    let hastaStr = `${hasta.getFullYear()}-${hasta.getMonth()+1}-${hasta.getDate()}`;
+
+    let url = `${this.urlMoviedb}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${this.apiKey}&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get(url)
+                      .map((res) => res.json());
+  }
 
 }
