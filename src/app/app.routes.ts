0dc2f5dca7802c3044f79e9fed6605c23c5d3c26
pantacha <pantacha@hotmail.com>
2018-04-04 17:21:56 +0200

    actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {SearchComponent} from './components/search/search.component';
+import {FilmComponent} from './components/film/film.component';
 
 const app_routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'search/:termino', component: SearchComponent },
+  { path: 'film/:id/:pag', component: FilmComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
